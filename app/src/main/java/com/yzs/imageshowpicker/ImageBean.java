6c5fa86296bb688f53bf09207b6d0ff0b3370a8a
ccj659 <569948231@qq.com>
2017-06-06 10:04:52 +0800

    Initial commit

---
+++ app/src/main/java/com/yzs/imageshowpicker/ImageBean.java
@@ -0,0 +1,33 @@
+package com.yzs.imageshowpicker;
+
+import com.yzs.imageshowpickerview.ImageShowPickerBean;
+
+/**
+ * Author 姚智胜
+ * Version V1.0版本
+ * Description:
+ * Date: 2017/4/10
+ */
+
+public class ImageBean extends ImageShowPickerBean {
+    private String url;
+    private int resId;
+
+    public ImageBean(String url) {
+        this.url = url;
+    }
+
+    public ImageBean(int resId) {
+        this.resId = resId;
+    }
+
+    @Override
+    public String setImageShowPickerUrl() {
+        return url;
+    }
+
+    @Override
+    public int setImageShowPickerDelRes() {
+        return resId;
+    }
+}
